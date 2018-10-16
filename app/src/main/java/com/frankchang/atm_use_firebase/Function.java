73e6e632bfc2ba0548208222a9019531cb41d5e3
lus33frank <lus33frank@gmail.com>
2018-10-17 00:02:56 +0800

    Commit : 4 主畫面設計，採用 RecyclerView 練習完成。

---
+++ app/src/main/java/com/frankchang/atm_use_firebase/Function.java
@@ -0,0 +1,24 @@
+package com.frankchang.atm_use_firebase;
+
+public class Function {
+
+    private String name;
+
+
+    public Function() {
+
+    }
+
+    public Function(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
--- app/src/main/java/com/frankchang/atm_use_firebase/MainActivity.java
+++ app/src/main/java/com/frankchang/atm_use_firebase/MainActivity.java
@@ -6,6 +6,8 @@ import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
@@ -15,8 +17,13 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    // 畫面元件
+    private RecyclerView recycler;
+    // 常數
     private static final int REQUEST_LOGIN = 123;
+    // 變數
     private boolean isLogon = false;
+    private String[] functions;
 
 
     @Override
@@ -30,10 +37,18 @@ public class MainActivity extends AppCompatActivity {
             startActivityForResult(loginIntent, REQUEST_LOGIN);
         }
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        findViews();    // 畫面元件連結
+        initData();     // 初始化資料
+    }
+
+    // 畫面元件連結
+    private void findViews() {
+        // Toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        // FloatingActionButton
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -41,19 +56,40 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        // Recycler
+        recycler = findViewById(R.id.recycler);
+        recycler.setHasFixedSize(true);     // 是不是固定大小
+        recycler.setLayoutManager(new LinearLayoutManager(this));   // 內部使用的容器類型
     }
 
+    // 初始化資料
+    private void initData() {
+        // 取得陣列資料
+        functions = getResources().getStringArray(R.array.functions);
+        // 設定 RecyclerView
+        FunctionAdapter adapter = new FunctionAdapter(this);
+        recycler.setAdapter(adapter);
+    }
+
+    // 轉頁回傳值處理
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
+        // 判斷登入結果
         if (requestCode == REQUEST_LOGIN) {
             if (resultCode == RESULT_OK) {
+                // 成功
                 Toast.makeText(this, "登入成功！", Toast.LENGTH_SHORT).show();
+
             } else if (resultCode == RESULT_CANCELED) {
+                // 失敗
                 Toast.makeText(this, "登入失敗！", Toast.LENGTH_SHORT).show();
                 finish();
+
             } else {
+                // 結束
                 finish();
             }
         }
